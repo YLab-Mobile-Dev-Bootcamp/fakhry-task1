bb8f48605da45bb91820c1cf19b39f923d1fc1b9
A. Muh. Fakhry Mubarak <fakhrymubarak@gmail.com>
2021-08-22 22:46:23 +0700

    feat: redesign movie_details_page.dart

--- movie_app/lib/views/movies/movie_details_page.dart
+++ movie_app/lib/views/movies/movie_details_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_app/const/constants.dart';
 import 'package:movie_app/controller/movie_details_controller.dart';
+import 'package:movie_app/themes/app_theme.dart';
+import 'package:movie_app/themes/colors.dart';
+import 'package:movie_app/themes/dimens.dart';
 
 class DetailsPage extends StatelessWidget {
   DetailsPage({Key? key}) : super(key: key);
@@ -10,156 +14,225 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Details of the Movie")),
-      body: SingleChildScrollView(
-        child: Obx(
-          () => _controller.movieDetails.value.title == null
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : NotificationListener<OverscrollIndicatorNotification>(
-                  onNotification: (OverscrollIndicatorNotification overscroll) {
-                    overscroll.disallowGlow();
-                    return true;
-                  },
-                  child: Scrollbar(
-                    isAlwaysShown: GetPlatform.isWeb || GetPlatform.isDesktop
-                        ? true
-                        : false,
-                    child: Center(
-                      child: Container(
-                        width: 1600,
-                        child: LayoutBuilder(
-                          builder: (context, constraint) {
-                            if (constraint.maxWidth <= 600) {
-                              return _columnView();
-                            } else if (constraint.maxWidth <= 1200) {
-                              return _columnView();
-                            } else {
-                              return _columnView();
-                            }
-                          },
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      home: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              child: Obx(
+                () => _controller.movieDetails.value.title == null
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : NotificationListener<OverscrollIndicatorNotification>(
+                        onNotification:
+                            (OverscrollIndicatorNotification overscroll) {
+                          overscroll.disallowGlow();
+                          return true;
+                        },
+                        child: Scrollbar(
+                          isAlwaysShown:
+                              GetPlatform.isWeb || GetPlatform.isDesktop
+                                  ? true
+                                  : false,
+                          child: Center(
+                            child: Container(
+                              width: 1600,
+                              child: LayoutBuilder(
+                                builder: (context, constraint) {
+                                  if (constraint.maxWidth <= 600) {
+                                    return _bodyWidget(context);
+                                  } else if (constraint.maxWidth <= 1400) {
+                                    return _bodyWidget(context);
+                                  } else {
+                                    return _bodyWidget(context);
+                                  }
+                                },
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
 
-  Widget _columnView() {
+  Widget _bodyWidget(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Container(
-          margin: EdgeInsets.all(8),
-          child: Row(
-            children: <Widget>[
-              Flexible(
-                flex: 1,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  child: Image.network(
-                    BASE_IMAGE + _controller.movieDetails.value.posterPath!,
-                    height: 160,
-                    fit: BoxFit.cover,
+          height: MediaQuery.of(context).size.height / 2.4,
+          child: Stack(
+            fit: StackFit.passthrough,
+            children: [
+              Image.network(
+                BASE_IMAGE_ORIGINAL +
+                    _controller.movieDetails.value.backdropPath!,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.center,
+                    end: Alignment.bottomCenter,
+                    colors: <Color>[
+                      Color(0x0),
+                      Color(primaryBackground),
+                    ],
                   ),
                 ),
               ),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(8),
-                      child: Text(
-                        _controller.movieDetails.value.title!,
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                      ),
+              Positioned(
+                top: regularSpace,
+                left: regularSpace,
+                child: InkWell(
+                  onTap: () => Get.back(),
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Color(grayTransparent50),
+                      shape: BoxShape.circle,
                     ),
-                    Container(
-                      margin: EdgeInsets.all(8),
-                      child: Wrap(
-                        children: [
-                          Text(
-                            'Release Date:',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            _controller.movieDetails.value.releaseDate!.toString(),
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14,
-                            ),
-                          ),
-                        ],
-                      ),
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Color(primaryText),
+                      size: extraLargeFontSize,
                     ),
-                    Container(
-                      margin: EdgeInsets.only(left: 8, right: 8),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.star,
-                            color: Colors.deepOrangeAccent,
-                          ),
-                          SizedBox(
-                            width: 4,
-                          ),
-                          Text(
-                            _controller.movieDetails.value.voteAverage.toString(),
-                            style: TextStyle(
-                              color: Colors.deepOrangeAccent,
-                              fontSize: 16,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+              )
             ],
           ),
         ),
-        Divider(
-          height: 50,
-          thickness: 5,
-        ),
         Container(
-          padding: EdgeInsets.only(left: 8, right: 8),
-          child: ListTile(
-            title: Text(
-              "Overview",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.black,
-              ),
-            ),
-            subtitle: Text(
-              _controller.movieDetails.value.overview!,
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.black,
-              ),
+          child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(
+                regularSpace, smallSpace, regularSpace, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _titleWidget(),
+                _releaseAndGenreWidget(),
+                _overviewWidget(),
+              ],
             ),
           ),
+        )
+      ],
+    );
+  }
+
+  Widget _titleWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _textWidget(
+            _controller.movieDetails.value.title!, extraLargeFontSize, true),
+        SizedBox(height: smallSpace),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.access_time_sharp,
+              color: Color(primaryText),
+              size: regularFontSize,
+            ),
+            SizedBox(width: miniSpace),
+            _textWidget("${_controller.movieDetails.value.runtime!} minutes",
+                regularFontSize, false),
+            SizedBox(width: regularSpace),
+            Icon(
+              Icons.star,
+              color: Color(selectableText),
+              size: regularFontSize,
+            ),
+            _textWidget(
+                "${_controller.movieDetails.value.voteAverage!} "
+                "(${_controller.movieDetails.value.voteCount!})",
+                regularFontSize,
+                false),
+          ],
+        ),
+        SizedBox(height: smallSpace),
+        Divider(
+          color: Color(whiteTransparent50),
         ),
       ],
+    );
+  }
+
+  Widget _releaseAndGenreWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _textWidget("Release Date", largeFontSize, true),
+                  SizedBox(height: smallSpace),
+                  _textWidget(_controller.movieDetails.value.releaseDate!,
+                      regularFontSize, false)
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _textWidget("Genres", largeFontSize, true),
+                  SizedBox(height: smallSpace),
+                  _textWidget(
+                      _controller.movieDetails.value.genres!
+                          .map((value) => value.name)
+                          .toList()
+                          .join(" • "),
+                      regularFontSize,
+                      false)
+                ],
+              ),
+            )
+          ],
+        ),
+        SizedBox(height: smallSpace),
+        Divider(
+          color: Color(whiteTransparent50),
+        ),
+      ],
+    );
+  }
+
+  Widget _overviewWidget() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: smallSpace),
+        _textWidget("Overview", largeFontSize, true),
+        SizedBox(height: smallSpace),
+        _textWidget(
+            _controller.movieDetails.value.overview!, regularFontSize, false),
+        SizedBox(height: regularSpace),
+      ],
+    );
+  }
+
+  Widget _textWidget(String text, double size, bool isBold) {
+    return Text(
+      text,
+      style: GoogleFonts.lato(
+          fontSize: size,
+          fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+          color: Color(primaryText)),
     );
   }
 }
